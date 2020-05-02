c6fb75c951d97e369bacd6f6fb65ce95e95756b0
Denis Burko <ealightpawn@gmail.com>
2020-05-02 15:29:59 +0300

    daadad

--- src/main/java/menu/inlinemenu/createrow/CreateRow.java
+++
@@ -1,9 +0,0 @@
-package menu.inlinemenu.createrow;
-
-import menu.inlinemenu.InlineMenu;
-import menu.inlinemenu.KeyboardRowFactory;
-
-public interface CreateRow {
-    void createProducts(KeyboardRowFactory factoryRow, Integer page, Integer maxRows, InlineMenu inlineMenu, Long telegramId, String category, String findText);
-    void createPages(KeyboardRowFactory factoryRow, Integer page, Integer maxRows, Long telegramId, String category, String findText, InlineMenu inlineMenu);
-}
